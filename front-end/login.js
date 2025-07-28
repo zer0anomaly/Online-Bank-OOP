07896107f76128c3d0043374d91c7b8e5a69669a
zer0anomaly <zeroanomale@gmail.com>
2025-07-28 17:23:19 +0500

    Update

---
+++ front-end/login.js
@@ -0,0 +1,47 @@
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let submit = document.getElementById("submit")
+
+class Login {
+	constructor(username, password){
+		this.username = username;
+		this.password = password;
+	}
+
+	login_method(){
+		async function making_login(){
+			try {
+				const response = await fetch('http://localhost:3000/login', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({
+						username: this.username,
+						password: this.password
+					})
+				});
+
+				if (!response.ok){
+					throw new Error (`HTTP error! Status: ${response.status}`)
+				}
+				
+				const data = await response.json();
+
+				if (data.result.includes("success")){
+					window.location.href = "http://localhost:3000/main.html"
+					localStorage.setItem("twtToken", data.jwtToken);
+					sessionStorage.setItem("sessionStorage", data.sessionStorage);
+				}else{
+					alert("Something went wront, please try again.")
+				}
+				making_login()
+			}
+		}
+	}
+}
+submit.addEventListener("click", () => {
+	let request = new Login(username, password);
+
+	request.login_method()
+})
