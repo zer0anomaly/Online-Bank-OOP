a718567086b628018b89030e61918bddc7a932e0
zer0anomaly <zeroanomale@gmail.com>
2025-08-01 12:08:26 +0500

    Update

---
+++ back-end/controller/auth.controller.js
@@ -0,0 +1,22 @@
+const AuthService = require('../services/AuthService');
+
+class UserController {
+  async login(req, res) {
+    let body = '';
+
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+
+    req.on('end', async () => {
+      const { username, password } = JSON.parse(body);
+
+      const response = await AuthService.login(username, password);
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(response));
+    });
+  }
+}
+
+module.exports = new UserController();
