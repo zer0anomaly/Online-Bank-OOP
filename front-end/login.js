e28bea9a948dda209d6a2d9d33b0cbf4c362997a
zer0anomaly <zeroanomale@gmail.com>
2025-08-02 11:25:06 +0500

    Update

--- front-end/login.js
+++ front-end/login.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-	let username = document.getElementById("username");
-	let password = document.getElementById("password");
-	let submit = document.getElementById("submit");
+	const username = document.getElementById("username");
+	const password = document.getElementById("password");
+	const form = document.getElementById("login-form");
 
 	class Login {
 		constructor(username, password){
@@ -23,18 +23,18 @@ document.addEventListener("DOMContentLoaded", () => {
 				});
 
 				if (!response.ok){
-					throw new Error (`HTTP error! Status: ${response.status}`);
+					throw new Error(`HTTP error! Status: ${response.status}`);
 				}
 				
 				const data = await response.json();
 
 				if (data.message.includes("Login successful")){
-					localStorage.setItem("twtToken", data.jwtToken);
-					sessionStorage.setItem("sessionStorage", data.sessionStorage);
+					// localStorage.setItem("twtToken", data.jwtToken);
+					// sessionStorage.setItem("sessionStorage", data.sessionStorage);
 					window.location.href = "http://localhost:3000/main.html";
-				}else if (data.message.includes("Login failed")){
+				} else if (data.message.includes("Login failed")) {
 					alert("Your email or password is wrong, please try again.");
-				}else {
+				} else {
 					alert("Something went wrong...");
 				}
 			} catch (error) {
@@ -44,7 +44,8 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 	}
 
-	submit.addEventListener("click", () => {
+	form.addEventListener("submit", (e) => {
+		e.preventDefault(); // prevent form from reloading page
 		const request = new Login(username.value, password.value);
 		request.login_method();
 	});
