f1e502d0c444ceef8bad4d05e6dd30d782b3f9b6
zer0anomaly <zeroanomale@gmail.com>
2025-08-05 11:25:18 +0500

    Update

--- back-end/controller/auth.controller.js
+++ back-end/controller/auth.controller.js
@@ -3,13 +3,13 @@ const authService = require('../services/auth.service');
 const loginUser = async (req, res) => {
 	try {
 		const { email, password } = req.body;
-		const result = await authService.loginUser(email, password);
+		const token = await authService.loginUser(email, password);
 
-		if (!result) {
+		if (!token) {
 			return res.status(401).json({ message: "Invalid email or password" });
 		}
 
-		res.status(200).json({ message: "Login successful", data: result });
+		res.status(200).json({ message: "Login successful", token });
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
