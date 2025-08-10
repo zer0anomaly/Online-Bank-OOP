9c6f549ccce8993302bb00c107c636a6562eebb3
zer0anomaly <zeroanomale@gmail.com>
2025-08-10 16:52:24 +0500

    Update

--- back-end/server.js
+++ back-end/server.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 const authRoutes = require('./routes/auth.routes');
 const registerRoutes = require('./routes/register.routes');
+const balanceRoutes = require('./routes/userinfo/balance.route');
 
 const app = express();
 const frontEndPath = path.join(__dirname, '../front-end');
@@ -16,7 +17,8 @@ app.use(express.static(frontEndPath));
 
 // Routes
 app.use('/auth', authRoutes);      
-app.use('/register', registerRoutes);    
+app.use('/register', registerRoutes); 
+app.use('/userinfo', balanceRoutes);   
 
 app.listen(3000, () => {
   console.log('Server running at http://localhost:3000');
