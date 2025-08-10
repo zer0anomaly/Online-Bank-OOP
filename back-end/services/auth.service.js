a0435d7b36490452542ce4928a7034a77392d263
zer0anomaly <zeroanomale@gmail.com>
2025-08-10 16:52:44 +0500

    Update

--- back-end/services/auth.service.js
+++ back-end/services/auth.service.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const jwt = require('jsonwebtoken');		
 const JWT_SECRET = 'WOUBALUBADUBDUB'
 
 class AuthService {
