7dbd4189bfc16e2da959c8ad21d3d9f409628c4f
zer0anomaly <zeroanomale@gmail.com>
2025-08-11 22:33:52 +0500

    Update

--- back-end/services/userinfo/balance.service.js
+++ back-end/services/userinfo/balance.service.js
@@ -2,25 +2,41 @@ const fs = require('fs').promises;
 const path = require('path');
 
 class ShowingBalance {
-	constructor() {
-		this.userFilePath = path.join(__dirname, '../../data/userinfo/balance.json');
-	}
+  constructor(email) {
+    this.email = email;
+  }
 
-	async readBalance() {
-		try {
-			const data = await fs.readFile(this.userFilePath, 'utf-8');
-			return JSON.parse(data);
-		} catch (err) {
-			console.error("Error reading balance file:", err);
-			return [];
-		}
-	}
+  async readBalance() {
+    try {
+      const safeEmail = this.email.replace(/[@.]/g, "_") + ".json";
+      const userFilePath = path.join(__dirname, `../../data/userinfo/balance/${safeEmail}`);
+      const data = await fs.readFile(userFilePath, 'utf-8');
+      return JSON.parse(data);
+    } catch (err) {
+      console.error("Error reading balance file:", err);
+      return null;  // Return null if file not found
+    }
+  }
 
-	async methodShowingBalance(email) {
-		const allData = await this.readBalance();
-		const user = allData.find(u => u.email === email);
-		return user ? user.balance : null;
-	}
+  async methodShowingBalance() {
+  const allData = await this.readBalance();
+  console.log("Loaded data:", allData);
+  console.log("Searching for email:", this.email);
+
+  if (!allData || !Array.isArray(allData)) {
+    console.error("Data is empty or not an array");
+    return null;
+  }
+
+  const user = allData.find(u => u.email === this.email);
+  if (!user) {
+    console.error("User not found in data");
+  }
+
+  return user ? user.balance : null;
 }
+
+}
+
 
 module.exports = ShowingBalance;
