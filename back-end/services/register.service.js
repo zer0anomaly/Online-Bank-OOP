fcbc32fadc853d7d8f0ad0ab2554c6687655ee1b
zer0anomaly <zeroanomale@gmail.com>
2025-08-11 22:33:28 +0500

    Update

--- back-end/services/register.service.js
+++ back-end/services/register.service.js
@@ -3,6 +3,8 @@ const path = require('path');
 const bcrypt = require('bcrypt');
 
 const path_for_users = path.join(__dirname, '../data/users.json');
+const path_for_transactions = path.join(__dirname, "../data/userinfo/transactions");
+const path_for_balance = path.join(__dirname, "../data/userinfo/balance")
 
 class RegisterService {
 	constructor(full_name, user_name, email, password) {
@@ -23,6 +25,51 @@ class RegisterService {
 		}
 	}
 
+	async userTransactionFile() {
+	    try {
+	        const safeEmail = this.email.replace(/[@.]/g, "_"); 
+	        const userFilePath = path.join(path_for_transactions, `${safeEmail}.json`);
+
+	        await fs.mkdir(path_for_transactions, { recursive: true });
+
+	        const initialData = [{
+	            date: new Date().toISOString(), 
+	            email: this.email,
+	            recent_transaction: null,
+	            type: null 
+	        }];
+
+	        await fs.writeFile(userFilePath, JSON.stringify(initialData, null, 2));
+
+	        console.log(`Transaction file created: ${userFilePath}`);
+	    } catch (err) {
+	        console.error("Error creating transaction file:", err);
+	    }
+	}
+	
+
+	async zeroBalanceCreation() {
+	    try {
+	        const safeEmail = this.email.replace(/[@.]/g, "_");
+	        const userBalanceCreation = path.join(path_for_balance, `${safeEmail}.json`);
+
+	        await fs.mkdir(path_for_balance, { recursive: true });
+
+	        const initialData = [{
+	            email: this.email,
+	            balance: 0
+	        }];
+
+	        await fs.writeFile(userBalanceCreation, JSON.stringify(initialData, null, 2));
+
+	        console.log(`Balance file created: ${userBalanceCreation}`);
+	    } catch (err) {
+	        console.error("Error creating balance file:", err);
+	    }
+	}
+
+
+
 	async registering() {
 		const fileContent = await fs.readFile(path_for_users, 'utf-8');
 		const users = JSON.parse(fileContent);
@@ -36,6 +83,8 @@ class RegisterService {
 		});
 
 		await fs.writeFile(path_for_users, JSON.stringify(users, null, 2));
+		await this.userTransactionFile();
+		await this.zeroBalanceCreation();
 		return 'User added successfully!';
 	}
 }
