d59aff55a5eb159117c563083bb8aafa051d39bd
zer0anomaly <zeroanomale@gmail.com>
2025-08-15 17:22:01 +0500

    Update

--- back-end/controller/recent_transaction.controller.js
+++ back-end/controller/recent_transaction.controller.js
@@ -1,18 +1,30 @@
 const recentTransaction = require("../services/recent_transaction.service");
 
 class recentTransactionController {
-	static async transfer(req, res) {
-		const { email } = req.body;
+    static async getRecentTransactions(req, res) {
+        const { email } = req.body;
 
-		if (!email){
-			return res.status(400).json({
-				result: "fail",
-				message: "Invalid Input"
-			});
-		}
+        if (!email) {
+            return res.status(400).json({
+                result: "fail",
+                message: "Invalid Input"
+            });
+        }
 
-		try {
-			const result = await recentTransaction.
-		}
-	}
+        try {
+            const result = recentTransaction.getUserInfo(email);
+            if (!result) {
+                return res.status(404).json([]);
+            }
+            return res.json(result);
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({
+                result: "fail",
+                message: "Server Error"
+            });
+        }
+    }
 }
+
+module.exports = recentTransactionController;
